aff1191457d53259eaf3b3238baacaa28e6ebaa1
PanDaoxi <63720932+PanDaoxi@users.noreply.github.com>
2023-02-25 13:21:48 +0800

    Update 163MusicPlayer.js

--- 163MusicPlayer.js
+++ 163MusicPlayer.js
@@ -25,7 +25,7 @@ var songs = [
 	30064263,
 	34040716,
 ];
+
 function getMusic(){
-	return "https://music.163.com/outchain/player?type=2&id=" + songs[parseInt(Math.random() * songs.length)] + "&auto=0&height=32";
+	document.getElementById("my163musicplayer").src = "https://music.163.com/outchain/player?type=2&id=" + songs[parseInt(Math.random() * songs.length)] + "&auto=0&height=32";
 }
-document.getElementById("my163musicplayer").src = getMusic();
